8fbe629ca8749150ce9d91a2de2df5e64329a9f8
Anderson <anhtamcrew@gmail.com>
2020-03-24 15:47:30 +0700

    Add project 9 Gate

---
+++ Social Gaming/flutter_9gate/android/app/src/main/kotlin/com/a9gate/anderson/flutter_9gate/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.a9gate.anderson.flutter_9gate
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
